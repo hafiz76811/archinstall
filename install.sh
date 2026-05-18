8c3e0c6c33dcc80c166f460648fc142e245dd09f
hafiz <debian7681@gmail.com>
2026-05-19 00:52:55 +0700

    upload all

---
+++ install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Hubungkan files
+source programs/functions/functions
+source programs/functions/logos/styling
+
+# Clear dan tampilkan logo
+clear
+installing_logo
+sleep 2
+
+# Hentikan script jika ada error
+set -e
+
+# Update dan upgrade system
+echo 'Update and upgrade system...'
+sudo pacman -Syyu
+
+# Install banyak packages dasar
+echo -e "\nInstalling basic packages..."
+basic_packages=$(cat programs/packages/pacman/basic_packages)
+pacman_install ${basic_packages[@]}
+
+hardware_packages=$(cat programs/packages/pacman/hardware_packages)
+pacman_install ${hardware_packages[@]}
+
+additional_tools=$(cat programs/packages/pacman/additional_tools)
+pacman_install ${additional_tools[@]}
+
+basic_fonts=$(cat programs/packages/pacman/basic_fonts)
+pacman_install ${basic_fonts[@]}
+
+# Clear dan tampilkan logo hyprland
+clear
+hyprland_logo
+sleep 2
+
+# Pasang aur helper (yay)
+echo -e "\nInstalling aur helper..."
+source programs/functions/commands/aur_helper
+
+# Install semua packages hyprland
+echo 'Installing hyprland...';
+hypr_ecosystem=$(cat programs/desktop/hyprland/hypr_ecosystem)
+pacman_install ${hypr_ecosystem[@]}
+
+hypr_ecosystem_aur=$(cat programs/desktop/hyprland/hypr_ecosystem_aur)
+yay_install ${hypr_ecosystem_aur[@]}
+
+display_manager=$(cat programs/desktop/hyprland/display_manager)
+pacman_install ${display_manager[@]}
+
+other_packages=$(cat programs/desktop/hyprland/other_packages)
+pacman_install ${other_packages[@]}
+
+waybar_packages=$(cat programs/desktop/hyprland/waybar_packages)
+pacman_install ${waybar_packages[@]}
+
+multimedia_tools=$(cat programs/desktop/hyprland/multimedia_tools)
+pacman_install ${multimedia_tools[@]}
+
+# Gunakan konfigurasi hyprland
+echo -e "\nHyprland configurations..."
+source programs/functions/commands/hyprland
+
+# Mengatur systemd services
+echo -e "\nSet up systemd services..."; sleep 2
+services=$(cat programs/systemd/enable/services)
+systemctl_enable ${services[@]}
+
+user_services=$(cat programs/systemd/enable/user_services)
+systemctl_user_enable ${user_services[@]}
+
+service=$(cat programs/systemd/disable/service)
+systemctl_disable ${service[@]}
+
+# Jalankan konfigurasi lain
+echo -e "\nAdditional configurations..."
+source programs/functions/commands/other
+
+echo "";
+
+# Reboot system setelah siap
+read -p "Reboot system now? [Y/n] " reboot
+if [[ $reboot == "y" ]]; then
+  systemctl reboot
+fi
